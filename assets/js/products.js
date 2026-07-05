d313d291984e46981939e6ea8e629c0df8649357
Vanita <vanitadua.2023@gmail.com>
2026-07-05 14:26:38 +0530

    Create centralized product data using JavaScript objects

---
+++ assets/js/products.js
@@ -0,0 +1,114 @@
+/* ===========================================================
+   Paper Petals
+   products.js
+
+   Stores all stationery products used throughout the website.
+=========================================================== */
+
+const products = [
+
+    {
+        id:101,
+        name:"Floral Dream Journal",
+        category:"Journals",
+        brand:"Paper Petals",
+        description:
+        "Premium floral hardcover journal with smooth ivory pages.",
+        price:399,
+        discount:20,
+        rating:4.9,
+        stock:40,
+        image:"../assets/images/products/floral-journal.jpg",
+
+        features:[
+            "Hard Cover",
+            "120 GSM Paper",
+            "Elastic Band",
+            "Ribbon Bookmark"
+        ]
+    },
+
+    {
+        id:102,
+        name:"Pastel Spiral Notebook",
+        category:"Notebooks",
+        brand:"Deli",
+        description:
+        "Cute spiral notebook perfect for school and office.",
+        price:249,
+        discount:15,
+        rating:4.8,
+        stock:70,
+        image:"../assets/images/products/pastel-notebook.jpg",
+
+        features:[
+            "A5 Size",
+            "200 Pages",
+            "Premium Paper",
+            "Soft Cover"
+        ]
+    },
+
+    {
+        id:103,
+        name:"Macaron Gel Pen Set",
+        category:"Gel Pens",
+        brand:"Zebra",
+        description:
+        "Smooth writing colorful gel pens.",
+        price:349,
+        discount:10,
+        rating:5.0,
+        stock:80,
+        image:"../assets/images/products/gel-pens.jpg",
+
+        features:[
+            "Set of 10",
+            "Quick Dry Ink",
+            "Fine Tip",
+            "Pastel Colors"
+        ]
+    },
+
+    {
+        id:104,
+        name:"Kawaii Sticker Pack",
+        category:"Stickers",
+        brand:"Paper Petals",
+        description:
+        "Cute waterproof decorative stickers.",
+        price:199,
+        discount:25,
+        rating:4.8,
+        stock:120,
+        image:"../assets/images/products/stickers.jpg",
+
+        features:[
+            "50 Stickers",
+            "Waterproof",
+            "Gloss Finish",
+            "Cute Designs"
+        ]
+    },
+
+    {
+        id:105,
+        name:"Pastel Washi Tape Set",
+        category:"Washi Tape",
+        brand:"MT",
+        description:
+        "Decorative washi tape collection for journaling.",
+        price:299,
+        discount:15,
+        rating:4.9,
+        stock:60,
+        image:"../assets/images/products/washi-tape.jpg",
+
+        features:[
+            "10 Rolls",
+            "Easy Tear",
+            "Pastel Theme",
+            "Reusable"
+        ]
+    }
+];
